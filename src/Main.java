bdf041101b01a2276da82a63286fba02914823bd
AltherLago <ajsl@ic.ufal.br>
2023-01-10 15:25:42 -0300

    Father Class Conteudo

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main{
+
+    public static void main(String[] args){
+
+        cleanConsoleScreen();
+
+         
+    }
+
+    public static void cleanConsoleScreen(){
+
+        System.out.print("[H[2J");
+        System.out.flush();
+
+    }
+
+}
