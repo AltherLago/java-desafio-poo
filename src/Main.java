0aeeac07a2347121c3704d2f2da3e6744c38a52c
AltherLago <ajsl@ic.ufal.br>
2023-01-10 15:50:45 -0300

    Curso

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,17 @@
+import classes.Curso;
+
 public class Main{
 
     public static void main(String[] args){
 
         cleanConsoleScreen();
 
-         
+        Curso curso1 = new Curso();
+        curso1.setTitulo("curso java");
+        curso1.setDescricao("descricao curso java");
+        curso1.setCargaHoraria(8);
+
+        System.out.println(curso1);
     }
 
     public static void cleanConsoleScreen(){
